c35879570410020433cb7047554d09e5e99ff14b
Sheyvie <sheilayvi@gmail.com>
2023-09-13 17:44:01 +0300

    First Commit

---
+++ ToDoApp/ToDo/Models/TaskDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDo.Models
+{
+    public class TaskDto
+    {
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } =string.Empty;
+        public string Description { get; set; }= string.Empty;
+
+        public string Importance { get; set; }=string.Empty;
+
+    }
+}
